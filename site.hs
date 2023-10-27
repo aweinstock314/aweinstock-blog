afdfb00c47ed985e718f826c8b3af23ba160af38
Avi Weinstock <git@aweinstock.com>
2023-10-27 02:17:05 -0400

    Port CSAW finals 2015 writeup.

--- site.hs
+++ site.hs
@@ -16,7 +16,7 @@ feedConfiguration = FeedConfiguration
 
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "images/**" $ do
         route   idRoute
         compile copyFileCompiler
 
